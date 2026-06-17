82ca9abddace3e4d219239ca7c96cd19d707e49a
MoArham29 <arhamkalam321@gmail.com>
2026-06-17 23:39:04 +0100

    Create figures folder if it does not exist

---
+++ code/churn_analysis.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Create figures folder if it does not exist
+os.makedirs("figures", exist_ok=True)
